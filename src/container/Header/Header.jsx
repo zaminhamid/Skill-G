370ce42c53d4fe27be7a9298e65f66783f33c165
zaminhamid <xaminhamid@gmail.com>
2023-09-09 21:52:42 +0530

    Updated background

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,31 +3,67 @@ import React from 'react';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
-import imag from '../../assets/skill-g.jpeg'; 
+import imag from '../../assets/skill.png'; 
 
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Beyond Books  " />
       <h6 className="app__header-h1"></h6>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>From Components to Creations: The Robotic Odyssey 🌠
-      We Provide online/offline Robotic Courses,Robotic Kit and Much More </p>
-      <h4 style={{ color: 'white' ,marginTop: '1rem'}}>Nitians and IISC Initative</h4>
-               <a style={{ marginTop:'1rem' }} href="https://docs.google.com/forms/d/e/1FAIpQLSdrriLkDl3-v-nAuY6x_pBZGFgvQ28KTEAn8DCSu9Z2bXdFkg/viewform?usp=sf_link">
-   
-     <button>Click here to join free Webinar</button>
-</a>
- 
+      
+      
+      
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+  <h1 style={{ marginTop: '1rem', color: 'white', fontSize: '28px' }}>
+    From Components to Creations: The Robotic Odyssey 🌠
+  </h1>
+  <p style={{ color: 'white', fontSize: '18px', lineHeight: '1.5' }}>
+    Welcome to the world of robotics and innovation! At RoboTech Academy, we are
+    passionate about igniting the spark of creativity and curiosity in young
+    minds. Our mission is to empower individuals to explore the limitless
+    possibilities of robotics and technology.
+  </p>
+  <p style={{ color: 'white', fontSize: '18px', lineHeight: '1.5' }}>
+    Whether you're a beginner or an experienced enthusiast, we offer a wide
+    range of online and offline robotic courses designed to cater to all
+    skill levels. Dive into the exciting world of coding, electronics,
+    mechanical engineering, and more!
+  </p>
+  <p style={{ color: 'white', fontSize: '18px', lineHeight: '1.5' }}>
+    Our state-of-the-art robotic kits are carefully curated to provide you with
+    everything you need to build and program your robots. Get ready to bring
+    your wildest ideas to life with our comprehensive kits.
+  </p>
+  <p style={{ color: 'white', fontSize: '18px', lineHeight: '1.5' }}>
+    Join us on this incredible journey, and together, let's create the future
+    of technology. Your adventure starts here at RoboTech Academy!
+  </p>
+  <h4 style={{ color: 'white', marginTop: '1rem' }}>Nitians and IISC Initative</h4>
+  <a style={{ marginTop: '1rem' }} href="https://docs.google.com/forms/d/e/1FAIpQLSdrriLkDl3-v-nAuY6x_pBZGFgvQ28KTEAn8DCSu9Z2bXdFkg/viewform?usp=sf_link">
+    <button style={{ fontSize: '18px', padding: '10px 20px', backgroundColor: 'blue', color: 'white', border: 'none', cursor: 'pointer' }}>
+      Click here to join free Webinar
+    </button>
+  </a>
+</p>
+
 
       
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
     </div>
     
 
-    <div className="app__wrapper_img">
-    
-      <img src={imag} alt="header_img" />
-    </div>
-    
+ 
 
   </div>
 );
